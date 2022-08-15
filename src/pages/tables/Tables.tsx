29ce52569d70ebdc784834dd41c763aba847e3c2
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2022-08-15 16:45:26 +0300

    create table modal

--- src/pages/tables/Tables.tsx
+++ src/pages/tables/Tables.tsx
@@ -1,19 +1,32 @@
 import HearderMenu from 'Features/components/header-menu/HeaderMenu'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'Features/ui/button/Button'
 import Banner from 'Images/banner.svg'
 import Cup from 'Images/cup.svg'
 import TablesList from './tablesList/TablesList'
 import Arrow from 'Images/arrow.svg'
+import Modal from 'Features/ui/modal/Modal'
+import Input from 'Features/ui/input/Input'
 
 export type ITableProps = {
   tableId?: number
 }
-const createTable = (_props: any) => {
-  console.log("created")
-}
 
 const Tables = (props: ITableProps) => {
+  const [isModalOpened, setIsModalOpened] = useState(false)
+
+  const createTable = () => {
+    console.log("created")
+  }
+
+  const openModal = () => {
+    setIsModalOpened(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpened(false)
+  }
+
   return (
     <div className='p-5 w-full'>
       <HearderMenu />
@@ -25,9 +38,20 @@ const Tables = (props: ITableProps) => {
                 <h1>Available tables</h1>
               </div>
               <div>
-                <Button clicked={createTable}>Create a table</Button>
+                <Button clicked={openModal}>Create a table</Button>
               </div>
             </div>
+            <Modal title='Create table' open={isModalOpened} closeHandle={closeModal}>
+              <div className='grid gap-1 mb-5 grid-cols-1'>
+              <div className='grid grid-cols-2 p-5 gap-0'>
+                  <Input className='bg-magenta mb-5' label='Table name' />
+                  <Input className='bg-magenta mb-5' label='Game type' />
+                  <Input className='bg-magenta mb-5' label='Bets' />
+                  <Input className='bg-magenta mb-5' label='Players'/>
+             </div>
+                <Button clicked={createTable}>Create</Button>
+              </div>
+            </Modal>
             <TablesList tables={null}></TablesList>
           </div>
         </div>
@@ -39,7 +63,7 @@ const Tables = (props: ITableProps) => {
           </div>
           <table className='w-full table-auto'>
             <tr className='bg-blue h-10 border-2 border-dark-blue text-left'>
-              <th/>
+              <th />
               <th>
                 Player
                 <img className='h-4 pl-1 inline' src={Arrow} alt='user photo' />
@@ -50,12 +74,12 @@ const Tables = (props: ITableProps) => {
               </th>
             </tr>
             <tr className='bg-blue h-10 border-2 border-dark-blue hover:bg-magenta'>
-              <td/>
+              <td />
               <td>User name</td>
               <td>$1000</td>
             </tr>
             <tr className='bg-blue h-10 border-2 border-dark-blue hover:bg-magenta'>
-              <td/>
+              <td />
               <td>User name</td>
               <td>$50000</td>
             </tr>
