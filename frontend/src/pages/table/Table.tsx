b76c72d4a0b64cebaefd9722601970a231eabee9
ionetek <ivan@oviland.ru>
2022-09-02 19:03:08 +0300

    Updated

--- frontend/src/pages/table/Table.tsx
+++ frontend/src/pages/table/Table.tsx
@@ -57,7 +57,7 @@ const Table: FC = () => {
   //При первой отрисовке компонента
   useEffect(() => {
     //Устанавливаем WS соединение
-    const socket = io('http://localhost:8080/', { transports: ['websocket'] })
+    const socket = io('https://yandex-poker-back.herokuapp.com/', { transports: ['websocket'] })
 
     //Инициализируем контроллер для управления столом, прокинув туда WS, геттер и сеттер состояния стола
     const tc = new TableController(socket, gameState, setGameState)
