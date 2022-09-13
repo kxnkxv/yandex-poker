d22507a210d1b680986aea9e16115ccd33753da2
OUT-Zhila-AA <AAZhila@sberbank.ru>
2022-09-13 18:12:58 +0300

    55:Add webpack build

---
+++ frontend/webpack/webpack.config.common.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, '../src/components'),
+      core: path.resolve(__dirname, '../src/core'),
+      hooks: path.resolve(__dirname, '../src/hooks'),
+      images: path.resolve(__dirname, '../src/images'),
+      pages: path.resolve(__dirname, '../src/pages'),
+      styles: path.resolve(__dirname, '../src/styles'),
+      types: path.resolve(__dirname, '../src/types'),
+      utils: path.resolve(__dirname, '../src/utils'),
+      '@': path.resolve(__dirname, '../src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './www/index.html',
+      favicon: './www/favicon.svg',
+      filename: 'index.html',
+    }),
+  ]
+}
---
+++ frontend/webpack/webpack.config.prod.js
@@ -0,0 +1,58 @@
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack.config.common.js')
+
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const prodConfig = {
+  mode: 'production',
+  stats: 'errors-only',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'static/js/[name].[chunkhash].js',
+    chunkFilename: 'static/js/[name].[chunkhash].chunk.js',
+    assetModuleFilename: 'static/media/[name].[hash:8].[ext]',
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          filename: 'static/js/vendor.[contenthash].js',
+          reuseExistingChunk: true,
+        },
+        common: {
+          name: 'common',
+          chunks: 'all',
+          minChunks: 2,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    runtimeChunk: {
+      name: 'runtime',
+    },
+    emitOnErrors: false,
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerPlugin()],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+}
+module.exports = merge(common, prodConfig)
