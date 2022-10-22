8c915e4a410e9e767c79bd7843be1305b1e170e5
ionetek <ivan@oviland.ru>
2022-10-23 02:16:07 +0400

    Updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,7 +27,7 @@ console.log('CLIENT URL:', process.env.CLIENT_URL);
 )*/
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://aigpoker.ru');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
