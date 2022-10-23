13f5db4d9062825ea6726a6d44c88f683cbfe379
ionetek <ivan@oviland.ru>
2022-10-23 11:39:43 +0400

    Updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,10 +27,10 @@ console.log('CLIENT URL:', process.env.CLIENT_URL);
 )*/
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
